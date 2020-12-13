81ca94b1ae0c563161067c3d3da4b3f1ccfd48a2
zacharyyao404 <wy510262073@gmail.com>
2020-12-13 12:23:56 +1100

    change-like-hooks

--- likes/src/App.js
+++ likes/src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import React, { useState, useReducer} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const initialState={
+  likes:100,
+  dislikes:5,
 }
-
+const appReducer=(state,action)=>{
+  switch(action.type){
+    case "HANDLE_LIKE":
+    return {
+      ...state, likes:
+      state.likes+action.payload
+    }
+    case "HANDLE_DISLIKE":
+    return{
+      ...state, dislikes:
+      state.dislikes+action.payload
+    }
+    default:
+    return state
+  }
+}
+const App=()=>{
+  const [state,dispatch]
+  =useReducer(appReducer,initialState)
+  const {likes, dislikes}=state
+  const [status,setStatus]=useState(null)
+  
+  const handleClickLike=()=>{
+    if(status==="like")
+      {
+        setStatus(null)
+        dispatch({
+          type:"HANDLE_LIKE",
+          payload:-1
+        })
+      }else{
+        setStatus("like")
+        if(status==="dislike"){
+          dispatch({
+            type:"HANDLE_DISLIKE",
+            payload:-1
+          })
+        }
+        dispatch({
+          type:"HANDLE_LIKE",
+          payload:1
+        })
+      }
+  }
+  
+  const handleClickDislike=()=>{
+    if(status==="dislike")
+      {
+        setStatus(null)
+        dispatch({
+          type:"HANDLE_DISLIKE",
+          payload:-1
+        })
+      }else{
+        setStatus("dislike")
+        if(status==="like"){
+          dispatch({
+            type:"HANDLE_LIKE",
+            payload:-1
+          })
+        }
+        dispatch({
+          type:"HANDLE_DISLIKE",
+          payload:1
+        })
+      }
+  }
+  return(
+    <div className="container">
+      <button 
+        className={status==="like" ? "btn active":"btn"}
+        onClick={handleClickLike}>
+        赞
+        <span>{likes}</span>
+      </button>
+      <button 
+        className={status==="dislike"?"btn active":"btn"}
+        onClick=          {handleClickDislike}>
+        踩
+        <span>{dislikes}</span>
+      </button>
+    </div>
+   )
+}
 export default App;
